ecdacb44010cd21eebd9cf9e5b338cfd107d4c5d
thebrible <lisa.wieselsberger@gmx.de>
2018-12-05 21:37:19 +0100

    Initial commit

---
+++ Isometric/Assets/Scripts/EnemyMovement.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class EnemyMovement : MonoBehaviour {
+
+    public float EnemySpeed;
+    private Vector3 targetPosition;
+    private Vector3 PlayerPosistion;
+    private bool inRange;
+
+    public GameObject Enemy;
+    public GameObject Player;
+
+	// Use this for initialization
+	void Start () {
+        targetPosition = transform.position;
+        inRange = false;
+        Enemy = GameObject.FindWithTag("Enemy");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        float distance = Math.Abs(Vector3.Distance(transform.position, Player.transform.position));
+        if(distance < 10)
+        {
+            inRange = true;
+        }
+        else
+        {
+            inRange = false;
+        }
+        SetTargetPosition();
+
+        if(inRange)
+        {
+            MoveEnemy();
+        }
+        
+		
+	}
+
+    private void SetTargetPosition()
+    {
+        if (inRange)
+        {
+            targetPosition = Player.transform.position;
+        }
+        else
+        {
+            targetPosition = transform.position;
+        }
+        
+
+    }
+    private void MoveEnemy()
+    {
+        Enemy.transform.LookAt(targetPosition);
+        transform.position = Vector3.MoveTowards(transform.position, targetPosition, EnemySpeed * Time.deltaTime);
+    }
+}
---
+++ Isometric/Assets/Scripts/PlayerCharacter.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerCharacter : MonoBehaviour {
+
+    public float Playerspeed;
+    private Vector3 targetPosition;
+    private bool isMoving;
+    const int LEFT_MOUSE_BUTTON = 0;
+    const int RIGHT_MOUSE_BUTTON = 1;
+
+    public GameObject Player; 
+    
+
+    // Use this for initialization
+    void Start () {
+
+        targetPosition = transform.position;
+        isMoving = false;
+        Player = GameObject.FindWithTag("Player");
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetMouseButton(LEFT_MOUSE_BUTTON))
+        {
+            SetTargetPosition();
+        }
+
+        if (isMoving)
+        {
+            MovePlayer();
+        }
+
+        if (Input.GetMouseButtonUp(LEFT_MOUSE_BUTTON))
+        {
+            isMoving = false;
+        }
+    }
+
+    private void SetTargetPosition()
+    {
+        Plane plane = new Plane(Vector3.up, transform.position);
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        float point = 0f;
+
+        if (plane.Raycast(ray, out point))
+            targetPosition = ray.GetPoint(point);
+
+        isMoving = true;
+    }
+
+    private void MovePlayer()
+    {
+        Player.transform.LookAt(targetPosition);
+        transform.position = Vector3.MoveTowards(transform.position, targetPosition, Playerspeed * Time.deltaTime);
+        if (Player.transform.position == targetPosition)
+            isMoving = false;
+    }
+}
